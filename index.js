06ff7ab0e61e40321524f749a507e681860d0f20
Nikolay Mihaylov <n.mihaylov@abv.bg>
2021-01-27 22:08:59 +0200

    routes
    created routes.js to give app catches his breath, literally not to worry about handling requests.

--- index.js
+++ index.js
@@ -1,18 +1,11 @@
 const express = require('express');
+
 const config = require('./config/config');
-const handlebars = require('express-handlebars');
+const routes = require('./routes');
 const app = express();
 
-app.engine('hbs', handlebars({
-    extname: 'hbs'
-}))
+require('./config/express')(app);
 
-app.set('view engine', 'hbs'); 
-
-app.use(express.static('public'));
-
-app.get('/', (req, res) => {
-    res.render('home', {layout: false })
-})
+app.use(routes);
 
 app.listen(config['PORT'], () => console.log(`The server is running on port ${config['PORT']}`));
