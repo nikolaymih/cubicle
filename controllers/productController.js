3594b0f00f9c8fe6889f07af595d87c07a2f0764
Nikolay Mihaylov <n.mihaylov@abv.bg>
2021-01-28 22:12:06 +0200

    add details page

--- controllers/productController.js
+++ controllers/productController.js
@@ -10,5 +10,9 @@ router.get('/create', (req, res) => {
     res.render('create', {title: 'Browse'});
 })
 
+router.get('/details/:productId', (req, res) => {
+    res.render('details', {title: 'Product details'});
+})
+
 module.exports = router;
 
